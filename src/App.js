19681bdab8607de83d6299e66c43997c0e1ed47e
dejosemario <josemariaodumodu@gmail.com>
2022-07-29 17:19:32 +0100

    get the project ready. Clean the files not needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="container">
+        <header>
+          Advice #177
+        </header>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, earum libero eaque, magni voluptatum est, sed minus vel assumenda dolorum fugiat alias!
+        </main>
+       </div>
     </div>
   );
 }
