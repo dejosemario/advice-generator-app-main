390aad2949c8f41588bc8784ad7ed745b72ae5f0
dejosemario <josemariaodumodu@gmail.com>
2022-07-30 14:48:12 +0100

    add the message content state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import PatternDivider from "./pattern-divider-desktop.svg";
+import IconDice from "./icon-dice.svg";
 
 function App() {
+  const [content, setContent] = useState("");
+  
   return (
     <div className="App">
-       <div className="container">
-        <header>
-          Advice #177
-        </header>
-        <main>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, earum libero eaque, magni voluptatum est, sed minus vel assumenda dolorum fugiat alias!
-        </main>
-       </div>
+      <div className="Header">Advice #177</div>
+      <div className="Content">
+        <p>
+          {content}
+        </p>
+      </div>
+      <div className="Divider">
+        {/* adding svg file */}
+        <img src={PatternDivider} alt="Divider" width="340px" />
+      </div>
+      <div className="Btn__Dice">
+        <img src={IconDice} alt="Icon Click" />
+      </div>
     </div>
   );
 }
