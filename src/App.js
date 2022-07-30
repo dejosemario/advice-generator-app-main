fa9a8b22bbfc686cf9a78d1f4f81a1d2b33afac9
dejosemario <josemariaodumodu@gmail.com>
2022-07-30 16:17:55 +0100

    adjusting the styles and code to get the desired effects

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import IconDice from "./icon-dice.svg";
 
 function App() {
   const [content, setContent] = useState("Click on the buton for your daily advice!");
+  const [contCount, setContCount] = useState("177");
 
   async function handleClick(e){
     e.preventDefault();
     const url = "https://api.adviceslip.com/advice";
     const res = await fetch(url);
     const data = await res.json();
-    setContent(data.slip.advice);   
+    setContent(data.slip.advice);
+    setContCount(data.slip.id);
   }
   
   return (
     <div className="App">
-      <div className="Header">Advice #177</div>
+      <div className="Header">ADVICE #{contCount}</div>
       <div className="Content">
         <p>
-          {content}
+          "{content}"
         </p>
       </div>
       <div className="Divider">
@@ -27,7 +29,7 @@ function App() {
         <img src={PatternDivider} alt="Divider" width="340px" />
       </div>
       <div className="Btn__Dice" >
-        <img src={IconDice} alt="Icon Click" onClick={handleClick}/>
+        <img src={IconDice} alt="Icon Click" width="20px"onClick={handleClick}/>
       </div>
     </div>
   );
